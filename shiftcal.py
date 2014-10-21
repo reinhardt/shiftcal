eff347346a83bd9e21e8eaa7fc5ded71ba546be5
Manuel Reinhardt <reinhardt@syslab.com>
2014-10-21 07:52:22 +0100

    refactored to allow shift definitions

--- shiftcal.py
+++ shiftcal.py
@@ -5,31 +5,40 @@ from icalendar import Calendar, Event
 
 
 OFF, EARLY, LATE, NIGHT = (0, 1, 2, 3)
+DEFAULT_DEFINITIONS = {
+    OFF: None,
+    EARLY: ['080000', '160000'],
+    LATE: ['113000', '200000'],
+    NIGHT: ['203000', '074500'],
+}
 
 
 class ShiftCal(object):
-    def __init__(self, start_date, shifts):
+    def __init__(self, start_date, shifts, definitions=[]):
         self.start_date = start_date
         self.shifts = shifts
+        self.definitions = definitions or DEFAULT_DEFINITIONS
 
     def get_ical(self):
         cal = Calendar()
         adate = self.start_date
         for shift in self.shifts:
             event = Event()
-            if shift == EARLY:
-                event['dtstart'] = adate.strftime('%Y%m%dT080000')
-                event['dtend'] = adate.strftime('%Y%m%dT160000')
-            elif shift == LATE:
-                event['dtstart'] = adate.strftime('%Y%m%dT113000')
-                event['dtend'] = adate.strftime('%Y%m%dT200000')
-            elif shift == NIGHT:
-                event['dtstart'] = adate.strftime('%Y%m%dT203000')
-                event['dtend'] = (adate + timedelta(1)).strftime('%Y%m%dT074500')
-            elif shift != OFF:
+            if shift not in self.definitions:
                 print('Unknown shift: {}'.format(shift))
-                shift = OFF
-            if shift != OFF:
+                times = None
+            else:
+                times = self.definitions[shift]
+            if times is not None:
+                event['dtstart'] = adate.strftime(
+                    '%Y%m%dT{0}'.format(times[0]))
+                if int(times[0]) > int(times[1]):
+                    enddate = adate + timedelta(1)
+                else:
+                    enddate = adate
+                event['dtend'] = enddate.strftime(
+                    '%Y%m%dT{0}'.format(times[1]))
+
                 cal.add_component(event)
             adate += timedelta(1)
         return cal.to_ical()
